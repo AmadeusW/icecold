90aa47732ceadb546dd2e312d37b60bee9d77f8f
Amadeusz Wieczorek <git@amadeusw.com>
2019-02-13 23:30:53 -0800

    collisions

--- prototype/script.js
+++ prototype/script.js
@@ -3,8 +3,13 @@ const sceneWidth = 600;
 const sceneHeight = 800;
 const paddleWidth = sceneWidth * 1.2;
 const paddleStart = 750;
+const wallWidth = 50;
 const moveDelta = 3;
 const maxYDelta = 150;
+const frictionAtHole = 0.01;
+const friction = 0.001; // default is 0.1
+const victoryLabel = "victory";
+const defeatLabel = "defeat";
 
 // module aliases
 var Engine = Matter.Engine,
@@ -12,7 +17,8 @@ var Engine = Matter.Engine,
     World = Matter.World,
     Bodies = Matter.Bodies,
     Events = Matter.Events,
-    Body = Matter.Body;
+    Body = Matter.Body,
+    Detector = Matter.Detector;
 
 // create an engine
 var engine = Engine.create();
@@ -30,16 +36,26 @@ var render = Render.create({
 var ball = Bodies.circle(sceneWidth/2, paddleStart - 50, 15, 80);
 var paddle = Bodies.rectangle(sceneWidth/2, paddleStart, paddleWidth, 10, { isStatic: true });
 var ground = Bodies.rectangle(sceneWidth/2, sceneHeight, sceneWidth, 20, { isStatic: true });
-var wall1 = Bodies.rectangle(0, sceneHeight/2, 4, sceneHeight, { isStatic: true });
-var wall2 = Bodies.rectangle(sceneWidth, sceneHeight/2, 4, sceneHeight, { isStatic: true });
+var wall1 = Bodies.rectangle(-wallWidth/2, sceneHeight/2, wallWidth, sceneHeight, { isStatic: true });
+var wall2 = Bodies.rectangle(sceneWidth + wallWidth/2, sceneHeight/2, wallWidth, sceneHeight, { isStatic: true });
 
-ball.friction = 0.0001; //  default is 0.1
+var sensorVictory = Bodies.circle(200, 300, 2, { isSensor: true, isStatic: true });
+var sensorNearVictory = Bodies.circle(200, 300, 17, { isSensor: true, isStatic: true, label: victoryLabel });
+
+var sensorX = Bodies.circle(400, 300, 5, { isSensor: true, isStatic: true });
+var sensorNearX = Bodies.circle(400, 300, 20, { isSensor: true, isStatic: true, label: defeatLabel });
+
+ball.friction = friction;
 ball.density = 0.05; // default is 0.001
-paddle.friction = 0.0001; // default is 0.1
-engine.world.gravity.scale = 0.002; // default is 0.001
+paddle.friction = friction;
+engine.world.gravity.scale = 0.005; // default is 0.001
 
 // add all of the bodies to the world
-World.add(engine.world, [ball, paddle, ground, wall1, wall2]);
+World.add(engine.world, [
+    ball,
+    paddle,
+    ground, wall1, wall2,
+    sensorVictory, sensorNearVictory, sensorX, sensorNearX]);
 
 // run the engine
 Engine.run(engine);
@@ -50,6 +66,7 @@ Render.run(render);
 var y1 = paddleStart;
 var y2 = paddleStart;
 var leftUp = false, leftDown = false, rightUp = false, rightDown = false;
+var potentialEndBody = undefined;
 
 var move = function(d1, d2) {
     var newy1 = y1 - d1;
@@ -66,8 +83,21 @@ var move = function(d1, d2) {
     y2 = newy2;
 }
 
-var handlePressedKeys = function(event) {
+Events.on(engine, "beforeUpdate", function(event) {
     var d1 = 0, d2 = 0;
+    // Check end-game condition
+    if (potentialEndBody !== undefined) {
+        var rd = potentialEndBody.circleRadius - ball.circleRadius;
+        if (Math.abs(ball.position.x - potentialEndBody.position.x) < rd
+        && Math.abs(ball.position.y - potentialEndBody.position.y) < rd) {
+            if (potentialEndBody.label === victoryLabel) {
+                console.log("😸");
+            } else if (potentialEndBody.label === defeatLabel) {
+                console.log("💥");
+            }
+        }
+    }
+    // Handle pressed keys
     if (leftUp && !leftDown) {
         d1 = moveDelta;
     }
@@ -83,9 +113,51 @@ var handlePressedKeys = function(event) {
     if (d1 != 0 || d2 != 0) {
         move(d1, d2);
     }
-}
+});
 
-Events.on(engine, "beforeUpdate", handlePressedKeys);
+Events.on(engine, 'collisionStart', function(event) {
+    var pairs = event.pairs;
+    // Collision has started.
+    // When we collide with the hole, we want to track its reference to validate end-game condition
+    // When we collide with the center of the hole, we adjust friction of the ball
+    for (var i = 0, j = pairs.length; i != j; ++i) {
+        var pair = pairs[i];
+        // So far, the ball is consistently bodyA
+        if (pair.bodyB === sensorNearVictory) {
+            potentialEndBody = pair.bodyB;
+        }
+        else if (pair.bodyB === sensorVictory) {
+            ball.friction = frictionAtHole;
+        }
+        else if (pair.bodyB === sensorNearX) {
+            potentialEndBody = pair.bodyB;
+        }
+        else if (pair.bodyB === sensorX) {
+            ball.friction = frictionAtHole;
+        }
+    }
+});
+
+Events.on(engine, 'collisionEnd', function(event) {
+    var pairs = event.pairs;
+    
+    for (var i = 0, j = pairs.length; i != j; ++i) {
+        var pair = pairs[i];
+
+        if (pair.bodyB === sensorNearVictory) {
+            potentialEndBody = undefined;
+        }
+        else if (pair.bodyB === sensorVictory) {
+            ball.friction = friction;
+        }
+        else if (pair.bodyB === sensorNearX) {
+            potentialEndBody = undefined;
+        }
+        else if (pair.bodyB === sensorX) {
+            ball.friction = friction;
+        }
+    }
+});
 
 document.addEventListener('keydown', function(event) {
     switch (event.keyCode)
