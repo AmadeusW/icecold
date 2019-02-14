247a9fbbdebfc3d6e10b2bd09aedb29622663889
Amadeusz Wieczorek <git@amadeusw.com>
2019-02-13 21:42:25 -0800

    refines the scene

--- prototype/script.js
+++ prototype/script.js
@@ -1,8 +1,18 @@
+// Game constants
+const sceneWidth = 600;
+const sceneHeight = 800;
+const paddleWidth = sceneWidth * 1.2;
+const paddleStart = 750;
+const moveDelta = 3;
+const maxYDelta = 150;
+
 // module aliases
 var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
-    Bodies = Matter.Bodies;
+    Bodies = Matter.Bodies,
+    Events = Matter.Events,
+    Body = Matter.Body;
 
 // create an engine
 var engine = Engine.create();
@@ -11,18 +21,22 @@ var engine = Engine.create();
 var render = Render.create({
     canvas: document.getElementById('canvas'),
     options: {
-        width: 400,
-        height: 800,
+        width: sceneWidth,
+        height: sceneHeight,
     },
     engine: engine
 });
 
-// create two boxes and a ground
-var ball = Bodies.circle(200, 200, 20, 80);
-var paddle = Bodies.rectangle(200, 700, 500, 30, { isStatic: true });
-var ground = Bodies.rectangle(200, 800, 400, 20, { isStatic: true });
-var wall1 = Bodies.rectangle(0, 400, 1, 800, { isStatic: true });
-var wall2 = Bodies.rectangle(400, 400, 1, 800, { isStatic: true });
+var ball = Bodies.circle(sceneWidth/2, paddleStart - 50, 15, 80);
+var paddle = Bodies.rectangle(sceneWidth/2, paddleStart, paddleWidth, 10, { isStatic: true });
+var ground = Bodies.rectangle(sceneWidth/2, sceneHeight, sceneWidth, 20, { isStatic: true });
+var wall1 = Bodies.rectangle(0, sceneHeight/2, 4, sceneHeight, { isStatic: true });
+var wall2 = Bodies.rectangle(sceneWidth, sceneHeight/2, 4, sceneHeight, { isStatic: true });
+
+ball.friction = 0.0001; //  default is 0.1
+ball.density = 0.05; // default is 0.001
+paddle.friction = 0.0001; // default is 0.1
+engine.world.gravity.scale = 0.002; // default is 0.001
 
 // add all of the bodies to the world
 World.add(engine.world, [ball, paddle, ground, wall1, wall2]);
@@ -33,48 +47,90 @@ Engine.run(engine);
 // run the renderer
 Render.run(render);
 
-var angle = 0;
-var angleDelta = 0.1;
-var maxAngle = 10;
+var y1 = paddleStart;
+var y2 = paddleStart;
+var leftUp = false, leftDown = false, rightUp = false, rightDown = false;
+
+var move = function(d1, d2) {
+    var newy1 = y1 - d1;
+    var newy2 = y2 - d2;
+    var yDelta = newy1 - newy2;
+    if (Math.abs(yDelta) >= maxYDelta)
+        return;
+
+    var midpoint = (newy1 + newy2) / 2;
+    var angle  = Math.tan(-yDelta / paddleWidth)
+
+    Body.set(paddle, { "angle": angle, "position": {x: sceneWidth/2, y: midpoint}}, 0);
+    y1 = newy1;
+    y2 = newy2;
+}
+
+var handlePressedKeys = function(event) {
+    var d1 = 0, d2 = 0;
+    if (leftUp && !leftDown) {
+        d1 = moveDelta;
+    }
+    if (leftDown && !leftUp) {
+        d1 = -moveDelta;
+    }
+    if (rightUp && !rightDown) {
+        d2 = moveDelta;
+    }
+    if (rightDown && !rightUp) {
+        d2 = -moveDelta;
+    }
+    if (d1 != 0 || d2 != 0) {
+        move(d1, d2);
+    }
+}
+
+Events.on(engine, "beforeUpdate", handlePressedKeys);
+
 document.addEventListener('keydown', function(event) {
     switch (event.keyCode)
     {
         case 74: // j = right down
-            //Matter.Body.set(paddle, "position", {x: 200, y: 300});
-            if (Math.abs(angle) >= maxAngle)
-                break;
-            angle -= angleDelta;
-            Matter.Body.set(paddle, "angle", angle);
+            rightDown = true;
             break;
         case 75: // k = right up
-            if (Math.abs(angle) >= maxAngle)
-                break;
-            angle += angleDelta;
-            Matter.Body.set(paddle, "angle", angle);
+            rightUp = true;
             break;
         case 85: // u = alternative right up
-            if (Math.abs(angle) >= maxAngle)
-                break;
-            angle += angleDelta;
-            Matter.Body.set(paddle, "angle", angle);
+            rightUp = true;
             break;
         case 83: // s = left down
-            if (Math.abs(angle) >= maxAngle)
-                break;
-            angle += angleDelta;
-            Matter.Body.set(paddle, "angle", angle);
+            leftDown = true;
             break;
         case 68: // d = left up
-            if (Math.abs(angle) >= maxAngle)
-                break;
-            angle -= angleDelta;
-            Matter.Body.set(paddle, "angle", angle);
+            leftUp = true;
             break;
         case 87: // w = alternative left up
-            if (Math.abs(angle) >= maxAngle)
-                break;
-            angle -= angleDelta;
-            Matter.Body.set(paddle, "angle", angle);
+            leftUp = true;
+            break;
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    switch (event.keyCode)
+    {
+        case 74: // j = right down
+            rightDown = false;
+            break;
+        case 75: // k = right up
+            rightUp = false;
+            break;
+        case 85: // u = alternative right up
+            rightUp = false;
+            break;
+        case 83: // s = left down
+            leftDown = false;
+            break;
+        case 68: // d = left up
+            leftUp = false;
+            break;
+        case 87: // w = alternative left up
+            leftUp = false;
             break;
     }
 });
