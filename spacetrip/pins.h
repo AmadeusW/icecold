55b43169dd40d4bfeee0aaa00c58d3b57d02ee57
Amadeusz Wieczorek <git@amadeusw.com>
2020-01-31 21:07:42 -0800

    attempt at moving servo

--- spacetrip/pins.h
+++ spacetrip/pins.h
@@ -6,11 +6,13 @@
 
 #define SensorAngle 0
 #define SensorLost 0
-#define SensorScored 0
+#define SensorScored 7
 #define SensorJoyAUp 4
 #define SensorJoyADown 5
 #define SensorJoyBUp 0
 #define SensorJoyBDown 0
+
+#define BallServo 6
 
 void setupPins()
 {
@@ -21,4 +23,6 @@ void setupPins()
     pinMode(STBY, OUTPUT);
     pinMode(SensorJoyAUp, INPUT);
     pinMode(SensorJoyADown, INPUT);
+    pinMode(SensorScored, INPUT);
+    pinMode(BallServo, OUTPUT);
 }
--- spacetrip/spacetrip.ino
+++ spacetrip/spacetrip.ino
@@ -1,26 +1,31 @@
 #include "pins.h"
 //<include "future.h"
+#include <Servo.h>
+
+Servo ballServo;
+bool joyAUp = false;
+bool joyADown = false;
+bool isScoring = false;
 
 void setup()
 {
    setupPins();
    startMotor();
+   ballServo.attach(BallServo);
    Serial.begin(9600); // set up debugging
 }
 
 void loop()
 {
    readSensors();
-   move();
+   act();
 }
-
-bool joyAUp = false;
-bool joyADown = false;
 
 void readSensors()
 {
    joyAUp = digitalRead(SensorJoyAUp) == HIGH;
    joyADown = digitalRead(SensorJoyADown) == HIGH;
+   isScoring = digitalRead(SensorScored) == HIGH;
 
    //Serial.print("Up: ");
    //Serial.print(joyAUpAnalog);
@@ -30,30 +35,39 @@ void readSensors()
    //Serial.print("Down: ");
    //Serial.print(joyADownAnalog);
    //Serial.print(" = ");
-   Serial.println(joyADown);
+   Serial.print(joyADown);
+   Serial.println(isScoring);
    //Serial.println("---");
 }
 
-void move()
+void act()
 {
+   if (isScoring)
+   {
+      score();
+   }
    if (joyAUp && joyADown)
    {
       // Error condition
       brake();
+      debugMessage(1);
    }
    else if (joyAUp)
    {
-      move(true);
+      digitalWrite(LED, HIGH);
+      move(true, 255);
    }
-   else if (joyADown)
+   else if (joyADown) // TODO: add another angle limiter
    {
-      move(false);
+      digitalWrite(LED, HIGH);
+      move(false, 255);
    }
    else
    {
       brake();
    }
    delay(50);
+   digitalWrite(LED, LOW);
 }
 
 void startMotor()
@@ -63,14 +77,35 @@ void startMotor()
 
 void brake()
 {
-   digitalWrite(LED, LOW);
    digitalWrite(AIN1, HIGH);
    digitalWrite(AIN2, HIGH);
 }
 
-void move(bool up)
+void score()
 {
+   analogWrite(BallServo, 10);
+   for (int i = 0; i < 5; i++)
+   {
+      digitalWrite(LED, LOW);
+      move(false, 50);
+      delay(100);
+      digitalWrite(LED, HIGH);
+      move(false, 50);
+      delay(100);
+   }
+   brake();
+   ballServo.write(160);
+   delay(1000);
+   Serial.print("B");
    digitalWrite(LED, HIGH);
+   ballServo.write(20);
+   delay(1000);
+   Serial.println("C");
+   digitalWrite(LED, LOW);
+}
+
+void move(bool up, int speed)
+{
    if (up)
    {
       digitalWrite(AIN1, HIGH);
@@ -81,5 +116,17 @@ void move(bool up)
       digitalWrite(AIN1, LOW);
       digitalWrite(AIN2, HIGH);
    }
-   analogWrite(PWMA, 255);
+   analogWrite(PWMA, speed);
+}
+
+void debugMessage(int code)
+{
+  for (int i = 0; i < code; i++)
+  {
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+    delay(100);
+  }
+  delay(300);  
 }
