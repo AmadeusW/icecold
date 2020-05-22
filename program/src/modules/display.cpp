3757b2eacf4a339cc710d18805ec857ca1850e04
Amadeusz Wieczorek <git@amadeusw.com>
2020-05-21 23:15:42 -0700

    fix display, add test pattern

--- program/src/modules/display.cpp
+++ program/src/modules/display.cpp
@@ -11,7 +11,7 @@ void Display::Setup()
     pinMode(PinSerialClock, OUTPUT);
     pinMode(PinSerialLatch, OUTPUT);
     pinMode(PinSerialData, OUTPUT);
-    this->TestSequence();
+    this->Test();
     this->SetLevel(0);
     this->SetHealth(0);
     this->Write();
@@ -32,17 +32,31 @@ void Display::SetHealth(int health)
     this->health = health;
 }
 
-void Display::TestSequence()
+void Display::Test()
 {
-    for (int i = 0; i < 8; i++)
+    long testArray[11];
+    testArray[0] = 0x000; // 0000
+    testArray[1] = 0x001; // 0001
+    testArray[2] = 0x003; // 0011
+    testArray[3] = 0x007; // 0111
+    testArray[4] = 0x00F; // 1111
+    testArray[5] = 0x01F;
+    testArray[6] = 0x03F;
+    testArray[7] = 0x07F;
+    testArray[8] = 0x0FF;
+    testArray[9] = 0x1FF;
+    testArray[10] = 0x3FF;
+    for (int i = 0; i < 11; i++)
     {
-        byte code = 1 << i;
+        long testCode = testArray[i];
+        byte transmission1 = testCode & 0xFF;
+        byte transmission2 = ((testCode & 0x300) >> 8) | ((testCode << 2) & 0xFC);
+
         digitalWrite(PinSerialLatch, 0); // Clear
-        shiftOut(PinSerialData, PinSerialClock, LSBFIRST, code);
-        shiftOut(PinSerialData, PinSerialClock, LSBFIRST, code);
-        shiftOut(PinSerialData, PinSerialClock, LSBFIRST, code);
+        shiftOut(PinSerialData, PinSerialClock, MSBFIRST, transmission2);
+        shiftOut(PinSerialData, PinSerialClock, MSBFIRST, transmission1);
         digitalWrite(PinSerialLatch, 1); // Latch
-        delay(50);
+        delay(30);
     }
 }
 
@@ -77,13 +91,13 @@ void Display::Write()
     long levelCode = levelArray[this->level];
     long healthCode = healthArray[this->health];
     // long targetCode = targetArray[this->level];
-    byte transmission1 = healthCode >> 8 & 0x03;
-    byte transmission2 = (healthCode >> 8 & 0x03) | (levelCode << 2 & 0xFC);
-    byte transmission3 = levelCode & 0xFF;
+    byte transmission1 = levelCode & 0xFF;
+    byte transmission2 = ((levelCode & 0x0300) >> 8) | (healthCode << 2 & 0xFC);
+    byte transmission3 = healthCode & 0xFF;
 
     digitalWrite(PinSerialLatch, 0); // Clear
-    shiftOut(PinSerialData, PinSerialClock, LSBFIRST, transmission3);
-    shiftOut(PinSerialData, PinSerialClock, LSBFIRST, transmission2);
-    shiftOut(PinSerialData, PinSerialClock, LSBFIRST, transmission1);
+    shiftOut(PinSerialData, PinSerialClock, MSBFIRST, transmission3);
+    shiftOut(PinSerialData, PinSerialClock, MSBFIRST, transmission2);
+    shiftOut(PinSerialData, PinSerialClock, MSBFIRST, transmission1);
     digitalWrite(PinSerialLatch, 1); // Latch
 }
--- program/src/modules/display.h
+++ program/src/modules/display.h
@@ -6,12 +6,12 @@
 class Display : public Module {
     int level;
     int health;
-    void TestSequence();
     public:
         virtual void Setup();
         virtual void Read();
         void SetLevel(int level);
         void SetHealth(int health);
+        void Test();
         void Write();
 };
 
