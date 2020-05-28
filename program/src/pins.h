ce8ee9f08205410dc0d56eda4b8cf2369d2373e4
Amadeusz Wieczorek <git@amadeusw.com>
2020-05-27 23:27:40 -0700

    update pins

--- program/src/pins.h
+++ program/src/pins.h
@@ -3,13 +3,15 @@
 
 #include "Arduino.h"
 
-#define SensorAngle 0
-#define SensorLost 0
-#define SensorBallDown 7
-#define SensorJoyAUp 4
-#define SensorJoyADown 5
-#define SensorJoyBUp 0
-#define SensorJoyBDown 0
+#define SensorJoyAUp 4 // green
+#define SensorJoyADown 5 // yellow
+#define SensorJoyBUp 6 // blue
+#define SensorJoyBDown 7 // yellow
+#define OutputSerialClock 8 // orange
+#define OutputSerialLatch 9 // orange
+#define SensorBallAtTarget 10 // white
+#define SensorBallAtBottom 11 // red
+#define OutputSerialData 12 // green
 
 #define PinServo 6
 
