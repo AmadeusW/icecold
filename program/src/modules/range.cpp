e2a08dbe3ae48c5efb122c7e0978edcf1150abb5
Amadeusz Wieczorek <git@amadeusw.com>
2020-06-08 08:52:03 -0700

    use correct voltages for the range meter

--- program/src/modules/range.cpp
+++ program/src/modules/range.cpp
@@ -9,31 +9,54 @@ VL53L0X lox1;
 
 void Range::Setup()
 {
+    // WARNING WARNING WARNING
+    // Device pins connected to PinRange0Enable and PinRange1Enable
+    // are not 5V tolerant. They are pulled up to 2.8V by the device
+    // or pulled down to 0V by us. To work around this, when we want
+    // to send a high signal, we set the pinMode to INPUT.
+    // This puts the pin in high impedance mode and device pulls it to 2.8V
+    //
+    // Background:
+    // This implementation uses a Polulu module, which does not perform
+    // level shifting. While the module did not get fried with 5V,
+    // communication over the I2C bus became impossible.
+    // Adafruit module does have a level shifting diode, but it costs 150%
+    // of the Polulu module. Therefore, for 2 modules, this hack saves $10.
+
     this->value0 = 0;
     this->initialized0 = false;
     this->value1 = 0;
     this->initialized1 = false;
     pinMode(PinI2CClock, OUTPUT);
     pinMode(PinI2CData, OUTPUT);
-    pinMode(PinRange0Enable, OUTPUT);
-    pinMode(PinRange1Enable, OUTPUT);
     // Program freezes if we initialize without the call to Wire.begin
     Wire.begin();
 
-    lox0.setTimeout(100);
-    lox1.setTimeout(100);
-    // Shut off sensors
+    // Reset the sensors
+    pinMode(PinRange0Enable, OUTPUT);
+    pinMode(PinRange1Enable, OUTPUT);
     digitalWrite(PinRange0Enable, 0);
+    digitalWrite(PinRange1Enable, 0);
     delay(10);
+    pinMode(PinRange1Enable, INPUT);
+    pinMode(PinRange1Enable, INPUT);
+    delay(10);
+    pinMode(PinRange0Enable, OUTPUT);
+    pinMode(PinRange1Enable, OUTPUT);
+    digitalWrite(PinRange0Enable, 0);
     digitalWrite(PinRange1Enable, 0);
     delay(10);
 
-    // Enable one sensor and adjust its address
-    digitalWrite(PinRange0Enable, 1);
+    // Enable sensor 0 and set it up
+    // WARNING: This pin is not 5V tolerant.
+    // Set pin to input so that the device pulls its pin up to 2.8V
+    pinMode(PinRange0Enable, INPUT);
     delay(10);
-    lox0.setAddress(0x28); // it's 0x29 by default
+    // Device 0 should be now enabled. Change its address from default 0x29
+    lox0.setAddress(0x30);
     if (lox0.init())
     {
+        lox0.setTimeout(100);
         Serial.println("Setup complete: Range 0");
         this->initialized0 = true;
     }
@@ -43,10 +66,16 @@ void Range::Setup()
         Serial.println("Error: Setup failed: Range 0");
     }
 
-    digitalWrite(PinRange1Enable, 1);
+    // Enable sensor 1 and set it up
+    // WARNING: This pin is not 5V tolerant.
+    // Set pin to input so that the device pulls its pin up to 2.8V
+    pinMode(PinRange1Enable, INPUT);
     delay(10);
+    // Device 1 should be now enabled. Change its address from default 0x29
+    lox1.setAddress(0x30);
     if (lox1.init())
     {
+        lox1.setTimeout(100);
         this->initialized1 = true;
         Serial.println("Setup complete: Range 1");
     }
@@ -58,7 +87,7 @@ void Range::Setup()
 
     uint8_t a0 = lox0.getAddress();
     uint8_t a1 = lox1.getAddress();
-    Serial.printf("Address0: %x --- Address1: %x \n", a0, a1);
+    Serial.printf("Information: Range0 at %x, Range1 at %x \n", a0, a1);
 }
 
 void Range::Read()
@@ -68,11 +97,11 @@ void Range::Read()
         this->value0 = lox0.readRangeSingleMillimeters();
         if (lox0.timeoutOccurred())
         {
-            Serial.println("Range 0 timed out.");
+            Serial.print("[R0 N/A].");
         }
         else
         {
-            Serial.printf("Range0 %d \n", this->value0);
+            Serial.printf("[R0 % 3d]", this->value0);
         }
     }
     if (this->initialized1)
@@ -80,11 +109,11 @@ void Range::Read()
         this->value1 = lox1.readRangeSingleMillimeters();
         if (lox1.timeoutOccurred())
         {
-            Serial.println("Range 1 timed out.");
+            Serial.print("[R1 N/A]");
         }
         else
         {
-            Serial.printf("Range1 %d \n", this->value1);
+            Serial.printf("[R1 % 3d]", this->value1);
         }
     }
 }
