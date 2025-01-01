a2a4ca1de3f2a7e30b11d0f308d99dac3e7193f8
Amadeusz Wieczorek <git@amadeusw.com>
2025-01-01 14:52:03 -0800

    trying to get the arduino part to build

---
+++ program/src/icecold-arcade/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+CPP_FILES=$(find ../common -name "*.cpp" | tr '\n' ';')
+echo "Files to be compiled:"
+echo $CPP_FILES | tr ';' '\n'
+arduino-cli compile -v --fqbn arduino:avr:mega \
+    --build-property "-std=gnu++11" \
+    --build-property "build.extra_flags=-I../common" \
+    --build-property "build.extra_source_files=$CPP_FILES" 
